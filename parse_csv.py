4700abd3692012eb34e0f1b9b9cfe52227e82f3d
LeonelCedillo <leonel.cs.979@gmail.com>
2025-09-27 15:37:11 -0700

    add Category in transaction.py

---
+++ parse_csv.py
@@ -0,0 +1,43 @@
+from transaction import Type, Transaction
+from datetime import datetime
+
+# raw row:
+# "Date","ReferenceNo.","Type","Description","Debit","Credit","CheckNumber","Balance"
+
+def filter_line(line):
+    txn = [x.strip('"') for x in line.split(",")]
+    if txn[2] == "DEBIT":
+        tranType = txn[4]
+    elif txn[2] == "CREDIT":
+        tranType = txn[5]
+    txn = [txn[0]] + txn[2:4] + [tranType] + [txn[7].strip('"\n')]
+    return txn
+
+
+def normalize_transaction(txn, prev_balance=None):
+    description = txn[2]
+    if not txn[3] or not txn[4]: 
+        raise ValueError(f"Missing amount or balance in row: {txn}")
+    amount = float(txn[3])
+    balance = float(txn[4])
+    tType = None
+
+    if txn[1] == "DEBIT": 
+        tType = Type.DEBIT
+        if amount > 0: 
+            amount = -amount
+    elif txn[1] == "CREDIT":
+        tType = Type.CREDIT
+        if amount < 0:
+            amount = -amount
+    if prev_balance != None:
+        expected = round(prev_balance + amount, 2)
+        if expected != round(balance, 2): 
+            raise ValueError(f"Balance mismatch: expected {expected}, got {balance}")
+    try: 
+        # string parse time: 8/18/2025 --> 2025-08-18 00:00:00
+        date_obj = datetime.strptime(txn[0], "%m/%d/%Y") 
+    except ValueError:
+        raise ValueError(f"Invalid date format in row: {txn}")
+
+    return Transaction(date_obj, tType, description, amount, balance)
--- transaction.py
+++ transaction.py
@@ -5,17 +5,28 @@ class Type(Enum):
     DEBIT = "withdrawal"
     CREDIT = "deposit"
 
+class Category(Enum):
+    PAYMENT = "Payment" # credit card, bill, subscription, zelle
+    TRANSFER = "Transfers" # checking to saving, viceversa, another bank account
+    INVESTMENT = "Investments" # fidelity, robinhood
+    DEPOSIT = "Deposit" # income, zelle, atm, School, 
+    OTHER = "Other"
+
 
 class Transaction:
-    def __init__(self, date, tType, description, amount, balance):
+    def __init__(self, date, type, description, amount, balance, category=Category.OTHER, subcategory=None):
         self.date = date
-        self.tType = tType
+        self.type = type
         self.description = description
         self.amount = amount
         self.balance = balance
+        self.category = category
+        self.subcategory = subcategory
 
     
     def __repr__(self):
         # string format time: 2025-08-18 00:00:00 --> 2025-8-18
         formated_date = self.date.strftime("%Y-%m-%d")
-        return f"Transaction({formated_date}, {self.tType}, {self.description}, {self.amount}, {self.balance})"
+        return (
+            f"Transaction({formated_date}, {self.type}, {self.description}, {self.amount}, {self.balance}, {self.category}, {self.subcategory})"
+        )
