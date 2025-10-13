e70766179deb710f0793c2b766be43a9289a4901
LeonelCedillo <leonel.cs.979@gmail.com>
2025-10-12 17:20:38 -0700

    progress

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ transactions = get_transaction_objects()
 transactions = sort_on_date(transactions, reverse=False)
 
 
-# init_balance = get_initial_balance(transactions)
-# fin_balance = get_final_balance(transactions)
-# print(f"Initial Balance: {format_amount(init_balance)}")
-# print(f"Final Balance: {format_amount(fin_balance)}")
-# print("")
+init_balance = get_initial_balance(transactions)
+fin_balance = get_final_balance(transactions)
+print(f"Initial Balance: {format_amount(init_balance)}")
+print(f"Final Balance: {format_amount(fin_balance)}")
+print("")
 
 # categories_dict = summarize_by_category(transactions)
 # category_summary_chart(categories_dict)
